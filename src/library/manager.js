29382c4cd38668886a4500d8bed6daba6e7c35d8
Manvel Khnkoyan <manvelkhnkoyan@MacBook-Pro.local>
2020-06-14 00:10:29 +0400

    Refactored Callbacks

--- src/library/manager.js
+++ src/library/manager.js
@@ -2,64 +2,82 @@
  * Event
  * @param {String} name
  */
-function Event(name) {
-  this.name = name;
-  this.callbacks = [];
+// eslint-disable-next-line max-classes-per-file
+class Event {
+  constructor(name) {
+    this.name = name
+    this.callbacks = []
+  }
 }
 
 /**
  * Manager
  */
-function Manager() {
-  const channelName = `${process.env.REACT_APP_NAME}-channel`;
-  this.events = {};
-  this.broadcastChannel = new BroadcastChannel(channelName);
-  this.broadcastChannel.onmessage = ({ data }) => {
-    this.dispatchEvent(data.name, data.args || null);
-  };
+class Manager {
+  constructor() {
+    const channelName = `${process.env.REACT_APP_NAME}-channel`
+    this.events = {}
+    this.uniqueId = 1;
+    this.broadcastChannel = new BroadcastChannel(channelName)
+    this.broadcastChannel.onmessage = ({ data }) => {
+      this.dispatchEvent(data.name, data.args || null)
+    }
+  }
+
+  /**
+   * Dispatching Event
+   * @param {String} name
+   * @param args
+   */
+  dispatchEvent(name, args) {
+    if (this.events[`on${name}`]) {
+      this.events[`on${name}`].callbacks.forEach(({ callback }) => {
+        callback(args)
+      })
+    }
+  }
+
+  /**
+   * Dispatching Event
+   * @param {String} name
+   * @param args
+   */
+  dispatchGlobalEvent(name, args) {
+    if (this.events[`on${name}`]) {
+      this.events[`on${name}`].callbacks.forEach(({ callback }) => {
+        callback(args)
+      })
+      this.broadcastChannel.postMessage({ name, args })
+    }
+  }
+
+  /**
+   * Adding Event Listener
+   * @param {String} name
+   * @param {Function} callback
+   */
+  addEventListener(name, callback) {
+    if (!this.events[name]) {
+      this.events[name] = new Event(name)
+    }
+    const id = this.uniqueId++;
+    this.events[name].callbacks.push({ id, callback })
+
+    return () => {
+      this.events[name].callbacks = this.events[name]
+        .callbacks.filter(callback => callback.id !== id)
+    }
+  }
+
+  /**
+   * Remmove Event Listener
+   * @param {String} name
+   */
+  removeEventListener(name) {
+    if (!this.events[name]) {
+      delete this.events[name]
+    }
+  }
 }
 
-/**
- * Dispatching Event
- * @param {String} name
- * @param args
- */
-Manager.prototype.dispatchEvent = function(name, args) {
-  if (this.events[`on${name}`]) {
-    this.events[`on${name}`].callbacks.forEach(function(callback) {
-      callback(args);
-    });
-  }
-};
-
-/**
- * Dispatching Event
- * @param {String} name
- * @param args
- */
-Manager.prototype.dispatchGlobalEvent = function(name, args) {
-  if (this.events[`on${name}`]) {
-    this.events[`on${name}`].callbacks.forEach(function(callback) {
-      callback(args);
-    });
-    this.broadcastChannel.postMessage({ name, args });
-  }
-};
-
-/**
- * Adding Event Listener
- * @param {String} name
- * @param {Function} callback
- */
-Manager.prototype.addEventListener = function(name, callback) {
-  if (!this.events[name]) {
-    this.events[name] = new Event(name);
-  }
-  this.events[name].callbacks.push(callback);
-
-  return () => {
-    delete this.events[name];
-  };
-};
-
-export default new Manager();
+export default new Manager()
